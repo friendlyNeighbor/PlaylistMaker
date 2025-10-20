cb48d0b9b576252f2dd4d0dd0df9bdc4bbc91d52
friendlyNeighbor <bitkinserejka@gmail.com>
2025-10-20 22:19:57 +0300

    PM

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,6 +12,7 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
      //   enableEdgeToEdge()
         setContentView(R.layout.activity_main)
     /*    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -18,5 +22,18 @@ class MainActivity : AppCompatActivity() {
         }
 
      */
+/*
+        val image = findViewById<Button>(R.id.button)
+
+        val imageClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на картинку!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        image.setOnClickListener(imageClickListener)
+
+*/
     }
+
 }
