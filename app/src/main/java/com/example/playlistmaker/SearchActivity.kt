c5c2d9b26b6948144ac755e70a36eb841bb77482
friendlyNeighbor <bitkinserejka@gmail.com>
2025-11-18 22:54:59 +0300

    PM sprint 9 review

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,18 +1,80 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
+import android.provider.Settings.Global.putString
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
+
+    private var text:String = TEXT_DEFAULT
+
+    companion object {
+        const val TEXT = "TEXT"
+        const val TEXT_DEFAULT = ""
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(TEXT, text)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-        val buttonBack = findViewById<Button>(R.id.search_back)
-
+        val buttonBack = findViewById<Toolbar>(R.id.search_back)
         buttonBack.setOnClickListener {
             finish()
+        }
+
+        if (savedInstanceState != null) {
+            text = savedInstanceState.getString(TEXT, TEXT_DEFAULT)
+        }
+
+        val inputEditText = findViewById<EditText>(R.id.edit_text)
+        inputEditText.setText(text)
+
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+
+        val clearButton = findViewById<ImageView>(R.id.clear_search)
+        clearButton.visibility = clearButtonVisibility(text)
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+            inputMethodManager?.hideSoftInputFromWindow(inputEditText.windowToken, 0)
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                text=s.toString()
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+        }
+
+        inputEditText.addTextChangedListener(simpleTextWatcher)
+
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
         }
     }
 }
