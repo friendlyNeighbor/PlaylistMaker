4e6302f9b55bd8b9394643ab0bcec4368f88e486
friendlyNeighbor <bitkinserejka@gmail.com>
2025-11-19 02:46:52 +0300

    Merge remote-tracking branch 'origin/dev' into dev
    # Conflicts:
    #	app/src/main/res/layout/activity_search.xml
    #	app/src/main/res/values/dimen.xml

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -23,7 +23,7 @@ class SettingsActivity : AppCompatActivity() {
 
         val buttonShare = findViewById<TextView>(R.id.share)
         buttonShare.setOnClickListener {
-            val message = "https://practicum.yandex.ru/android-developer/"
+            val message = getString(R.string.link)
             val intent = Intent(android.content.Intent.ACTION_SEND);
             intent.setType("text/plain");
             intent.putExtra(android.content.Intent.EXTRA_TEXT, message);
@@ -31,18 +31,18 @@ class SettingsActivity : AppCompatActivity() {
         }
         val buttonSupport = findViewById<TextView>(R.id.support)
         buttonSupport.setOnClickListener {
-            val text = "Спасибо разработчикам и разработчицам за крутое приложение!"
-            val subject = "Сообщение разработчикам и разработчицам приложения Playlist Maker»"
+            val text = getString(R.string.text_message)
+            val subject = getString(R.string.subject_message)
             val intent = Intent(Intent.ACTION_SENDTO)
             intent.data = Uri.parse("mailto:")
-            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf("RulezZzZzko@ya.ru"))
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.mail_adress)))
             intent.putExtra(Intent.EXTRA_TEXT, text)
             intent.putExtra(Intent.EXTRA_SUBJECT, subject)
             startActivity(intent)
             }
         val buttonAgreement = findViewById<TextView>(R.id.agreement)
         buttonAgreement.setOnClickListener {
-            val url = "https://yandex.ru/legal/practicum_offer/ru/"
+            val url = getString(R.string.link_offer)
             intent  = Intent(Intent.ACTION_VIEW)
             intent.setData(Uri.parse(url))
             startActivity(intent)
