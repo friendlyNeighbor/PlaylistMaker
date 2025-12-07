61cefc7b78fe15a193387d3c41b4a124cd6b07c7
friendlyNeighbor <bitkinserejka@gmail.com>
2025-12-07 22:17:55 +0300

    Спринт 11 (Ревью)

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,23 +1,40 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.updatePadding
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class SearchActivity : AppCompatActivity() {
+    private val iTunesService = RetrofitClient.iTunesService
+
+    private lateinit var inputEditText: EditText
+    private lateinit var buttonClear: ImageView
+    private lateinit var viewTracks: RecyclerView
+    private lateinit var viewErrorSearch: LinearLayout
+    private lateinit var viewErrorConnection: LinearLayout
+    private lateinit var buttonReload: View
 
     private var text:String = TEXT_DEFAULT
+    private val trackList:MutableList<Track> = mutableListOf()
+    private val tracksAdapter = TrackAdapter(trackList)
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
@@ -26,7 +43,9 @@ class SearchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        if (savedInstanceState != null) {
+            text = savedInstanceState.getString(TEXT, TEXT_DEFAULT)
+        }
         enableEdgeToEdge()
         setContentView(R.layout.activity_search)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.search)) { v, insets ->
@@ -34,48 +53,63 @@ class SearchActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        viewTracks = findViewById(R.id.recycler)
+        viewTracks.adapter = tracksAdapter
+        viewErrorSearch = findViewById(R.id.not_found)
+        viewErrorConnection = findViewById(R.id.connection_problem)
+        buttonReload = findViewById(R.id.reload)
 
-        setContentView(R.layout.activity_search)
-
-        val tracks: RecyclerView = findViewById(R.id.recycler)
-        tracks.adapter = tracksAdapter
 
         val buttonBack = findViewById<androidx.appcompat.widget.Toolbar>(R.id.search_back)
         buttonBack.setOnClickListener {
             finish()
         }
 
-        if (savedInstanceState != null) {
-            text = savedInstanceState.getString(TEXT, TEXT_DEFAULT)
-        }
-
-        val inputEditText = findViewById<EditText>(R.id.edit_text)
+        inputEditText = findViewById(R.id.edit_text)
         inputEditText.setText(text)
-
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
 
-        val clearButton = findViewById<ImageView>(R.id.clear_search)
-        clearButton.visibility = clearButtonVisibility(text)
-        clearButton.setOnClickListener {
+        buttonClear = findViewById(R.id.clear_search)
+        buttonClear.visibility = clearButtonVisibility(text)
+        buttonClear.setOnClickListener {
             inputEditText.setText("")
             inputMethodManager?.hideSoftInputFromWindow(inputEditText.windowToken, 0)
         }
 
-       val simpleTextWatcher = object : TextWatcher {
-
+        val simpleTextWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
-
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
             }
-
             override fun afterTextChanged(s: Editable?) {
                 text=s.toString()
-                clearButton.visibility = clearButtonVisibility(s)
+                buttonClear.visibility = clearButtonVisibility(s)
+                if (text.isEmpty()) {
+                    trackList.clear()
+                    setViewSearch(CLEAR)
+                }
             }
         }
         inputEditText.addTextChangedListener(simpleTextWatcher)
-    }
+
+        inputEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                if (text.isNotEmpty()) {
+                    searchTrack(text)
+                }
+                inputMethodManager?.hideSoftInputFromWindow(inputEditText.windowToken, 0)
+                true
+            } else {
+                false
+            }
+        }
+
+        buttonReload.setOnClickListener {
+            if (text.isNotEmpty()) {
+                searchTrack(text)
+            }
+        }
+    } // <- end of onCreate
 
     private fun clearButtonVisibility(s: CharSequence?): Int {
         return if (s.isNullOrEmpty()) {
@@ -84,42 +118,59 @@ class SearchActivity : AppCompatActivity() {
             View.VISIBLE
         }
     }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun setViewSearch(reason: String) {
+        tracksAdapter.notifyDataSetChanged()
+        viewErrorSearch.visibility = View.GONE
+        viewErrorConnection.visibility = View.GONE
+        viewTracks.visibility = View.GONE
+        when (reason) {
+            "CONNECTION_PROBLEM" -> viewErrorConnection.visibility = View.VISIBLE
+
+            "NOT_FOUND" -> viewErrorSearch.visibility = View.VISIBLE
+
+            "OK" -> viewTracks.visibility = View.VISIBLE
+        }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val restoredText = savedInstanceState.getString("search_text", "")
+        inputEditText.setText(restoredText)
+    }
+
+    private fun searchTrack(query: String) {
+        iTunesService.searchSong(query).enqueue(object : Callback<SearchResponse> {
+            override fun onResponse(call: Call<SearchResponse>, response: Response<SearchResponse>) {
+                if (response.isSuccessful) {
+                    val body = response.body()
+                    trackList.clear()
+                    body?.results?.forEach { result ->
+                        val formattedTime = SimpleDateFormat("mm:ss", Locale.getDefault())
+                            .format(result.trackTimeMillis)
+                        trackList.add(
+                            Track(result.trackName, result.artistName, formattedTime, result.artworkUrl100 )                  )
+                    }
+                    if (trackList.isEmpty()) {
+                        setViewSearch(NOT_FOUND)
+                    } else {
+                        setViewSearch(OK)
+                    }
+                }
+            }
+            override fun onFailure(call: Call<SearchResponse>, t: Throwable) {
+                setViewSearch(CONNECTION_PROBLEM)
+            }
+        })
+    }
+
     companion object {
         private const val TEXT = "TEXT"
         private const val TEXT_DEFAULT = ""
+        private const val CONNECTION_PROBLEM = "CONNECTION_PROBLEM"
+        private const val NOT_FOUND = "NOT_FOUND"
+        private const val OK = "OK"
+        private const val CLEAR = "CLEAR"
     }
-
-    private val track1=Track(
-         "Smells Like Teen Spirit",
-         "Nirvana",
-         "5:01",
-        "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-    )
-    private val track2=Track(
-        "Billie Jean",
-        "Michael Jackson",
-        "4:35",
-        "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
-    )
-    private val track3=Track(
-        "Stayin' Alive",
-        "Bee Gees",
-        "4:10",
-        "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
-    )
-    private val track4=Track(
-        "Whole Lotta Love",
-        "Led Zeppelin",
-        "5:33",
-        "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
-    )
-    private val track5=Track(
-        "Sweet Child O'Mine",
-        "Guns N' Roses",
-        "5:03",
-        "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
-    )
-    private val trackList:MutableList<Track> = mutableListOf(track1, track2, track3, track4, track5)
-    private val tracksAdapter = TrackAdapter(trackList)
-
 }
