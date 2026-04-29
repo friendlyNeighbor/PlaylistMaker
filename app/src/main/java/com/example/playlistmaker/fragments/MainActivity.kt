06ab8abb48bc8f45bbcf1f23b7759ad12b89a7f3
friendlyNeighbor <bitkinserejka@gmail.com>
2026-04-30 02:43:28 +0300

    Sprint 19 (Preparation)

--- app/src/main/java/com/example/playlistmaker/fragments/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/fragments/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.playlistmaker.mvvm.main.ui
+package com.example.playlistmaker.fragments
 
 import android.content.Intent
 import android.os.Bundle
@@ -6,21 +6,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.ActivityMainBinding
+import com.example.playlistmaker.mvvm.main.ui.MainState
+import com.example.playlistmaker.mvvm.main.ui.MainViewModel
 import com.example.playlistmaker.mvvm.media.ui.MediatekaActivity
 import com.example.playlistmaker.mvvm.search.ui.SearchActivity
 import com.example.playlistmaker.mvvm.settings.ui.SettingsActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val primaryState = MainState.WAIT
-    private val viewModel: MainViewModel by viewModel() {
-        parametersOf(primaryState)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,34 +34,12 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        viewModel.getLiveData().observe(this) {
-            when {
-                it == MainState.SEARCH -> {
-                    startActivity(Intent(this, SearchActivity::class.java))
-                    viewModel.resetState()
-                }
-                it == MainState.MEDIA -> {
-                    startActivity(Intent(this, MediatekaActivity::class.java))
-                    viewModel.resetState()
-                }
-                it == MainState.SETTINGS -> {
-                    startActivity(Intent(this, SettingsActivity::class.java))
-                    viewModel.resetState()
-                }
-            }
-        }
 
-        binding.buttonSearch.setOnClickListener {
-            viewModel.goToSearchActivity()
-        }
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment
+        val navController = navHostFragment.navController
 
-        binding.buttonMedia.setOnClickListener {
-            viewModel.goToMediatekaActivity()
-        }
-
-        binding.buttonSettings.setOnClickListener {
-            viewModel.goToSettingsActivity()
-        }
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setupWithNavController(navController)
 
     }
 }
---
+++ app/src/main/java/com/example/playlistmaker/fragments/MediatekaFragment.kt
@@ -0,0 +1,38 @@
+package com.example.playlistmaker.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.playlistmaker.databinding.FragmentMediatekaBinding
+import com.example.playlistmaker.databinding.FragmentPlaylistsBinding
+import com.example.playlistmaker.mvvm.media.ui.PlaylistsState
+
+class MediatekaFragment : Fragment() {
+
+    private var _binding: FragmentMediatekaBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMediatekaBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
